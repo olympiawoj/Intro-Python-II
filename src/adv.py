92f5286699aac0a699bbc3785f1ac2ace5c864aa
olympia <owojcik7@gmail.com>
2020-02-12 14:45:23 -0500

    Add __str__ to the player class

--- src/adv.py
+++ src/adv.py
@@ -1,26 +1,26 @@
-# from room import Room
+from room import Room
 from player import Player
 
 # Declare all the rooms
 
-# room = {
-#     'outside':  Room("Outside Cave Entrance",
-#                      "North of you, the cave mount beckons"),
+room = {
+    'outside':  Room("Outside Cave Entrance",
+                     "North of you, the cave mount beckons"),
 
-#     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-# passages run north and east."""),
+    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+passages run north and east."""),
 
-#     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-# into the darkness. Ahead to the north, a light flickers in
-# the distance, but there is no way across the chasm."""),
+    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+into the darkness. Ahead to the north, a light flickers in
+the distance, but there is no way across the chasm."""),
 
-#     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-# to north. The smell of gold permeates the air."""),
+    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+to north. The smell of gold permeates the air."""),
 
-#     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-# chamber! Sadly, it has already been completely emptied by
-# earlier adventurers. The only exit is to the south."""),
-# }
+    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+chamber! Sadly, it has already been completely emptied by
+earlier adventurers. The only exit is to the south."""),
+}
 
 
 # Link rooms together
@@ -40,14 +40,18 @@ from player import Player
 
 # Make a new player object that is currently in the 'outside' room.
 player_name = input("What is your name? ")
-print(player_name)
+player = Player(player_name, [])
+print(player)
+
+print(f"\n{player.name} in the {player.current_room} room")
 
 # Write a loop that:
 #
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
-#
+
+
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
 #
--- src/player.py
+++ src/player.py
@@ -6,3 +6,7 @@ class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = "outside"
+
+    def __str__(self):
+        # Method __str__ should return string, not print.
+        return f"The player {self.name} is in {self.current_room}"
