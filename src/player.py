e0be182a7e4d016519c6d03186c6b7cd23a7afd4
olympia <owojcik7@gmail.com>
2020-02-13 14:54:24 -0500

    Add add_item method to the Player class

--- src/player.py
+++ src/player.py
@@ -3,16 +3,26 @@
 
 
 class Player:
-    def __init__(self, name, current_room):
+    def __init__(self, name, current_room, inventory):
         """
         This is constructor function creates all needed variables for
         instantiation of a new player
         """
         self.name = name
         self.current_room = "outside"
+        self.inventory = []
 
     def set_room(self, current_room):
         self.current_room = current_room
+
+    def add_item(self, item):
+        """
+        This method adds an item to the player's inventory
+        """
+        if item not in self.inventory:
+            self.inventory.push(item)
+        else:
+            print("This item is already in your inventory")
 
     def __str__(self):
         """
