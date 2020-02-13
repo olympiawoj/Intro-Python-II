194c334cf5d9dcb538aca5add44f602bc08b5e38
olympia <owojcik7@gmail.com>
2020-02-13 14:13:51 -0500

    In Room class, add item_list as param and method to print all items in a room

--- src/room.py
+++ src/room.py
@@ -3,9 +3,17 @@
 
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, item_list):
         self.name = name
         self.description = description
+        self.item_list = item_list
+
+    def print_items(self):
+        """
+        Prints all items in the room's item_list
+        """
+        for item in self.item_list:
+            print(item)
 
     def __str__(self):
         return f'Room: {self.name}, Description: {self.description}'
